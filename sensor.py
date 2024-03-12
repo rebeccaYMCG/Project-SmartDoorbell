be946755961469471c5f4d2bcb7189fdb2857699
rebeccaYMCG <A00241382@mycambrian.ca>
2024-03-12 00:50:02 -0400

    Run subprocess on faceDetection and create button functionality.

--- sensor.py
+++ sensor.py
@@ -1,14 +1,20 @@
-from gpiozero import MotionSensor
+from gpiozero import MotionSensor, Button
 from datetime import datetime
 from time import sleep 
+import subprocess
 
-motionSensor = MotionSensor(23)
+motionSensor = MotionSensor(23) # these will be changed for actual GPIO pins
+button = Button(13)
 
 def captureImage():
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     imagePath = f"motion_capture_{timestamp}.jpg"
+    print(f"Capturing Image: {imagePath}")
 
-    #create a function that will tell individual that a pic was taken
+    # send to SQL database to then send to phone ?
+
+    # calls the face detection py
+    subprocess.run(['python', 'faceDetection.py', imagePath])
 
 try: 
     print("Motion detection system is active. Press Crtl+C to exit.")
@@ -17,6 +23,10 @@ try:
         if motionSensor.motion_detected:
             print("Motion Detected!")
             captureImage()
+
+        if button.is_pressed: 
+            print("Button Pressed!")
+            captureImage()
     sleep(2)
 
 except KeyboardInterrupt:
@@ -24,3 +34,4 @@ except KeyboardInterrupt:
 
 finally: 
     motionSensor.close()
+    button.close()
